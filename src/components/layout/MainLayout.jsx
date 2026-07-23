c8534a43528d38cb55d3d76e2ff5320a7a5c3bc2
SammyGironda <samy.gironda@gmail.com>
2026-07-22 22:55:16 -0400

    pantalla dashboard

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
-import Header from './Header';
 
 const MainLayout = () => {
   return (
     <div className="flex min-h-screen bg-[#F2F2F2]">
       <Sidebar />
       <div className="flex-1 flex flex-col ml-60 overflow-hidden">
-        <Header />
         <main className="flex-1 overflow-x-auto overflow-y-auto p-4">
           <div className="min-w-max">
             <Outlet />
